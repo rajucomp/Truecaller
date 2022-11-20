3964330fa74e27392f58438c7c3397e2c43c37b1
Raju Gupta <cs135017@mnnit.ac.in>
2022-11-20 13:46:56 +0100

    Added a comment.

--- src/main/java/org/example/StringUtilityFunctions.java
+++ src/main/java/org/example/StringUtilityFunctions.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class StringUtilityFunctions {
+
+    // A function that returns true if the string is null or empty.
     public static boolean isNullOrEmpty(String word) {
         return word == null || word.trim().isEmpty();
     }
